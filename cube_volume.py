8b7f1f5d0e18d905f651f71585374aa5f8712e8d
hiroto <kazamah@oregonstate.edu>
2021-05-03 13:45:33 -0700

    first commit

---
+++ cube_volume.py
@@ -0,0 +1,7 @@
+def cube(a):
+    return a ** 3
+"""
+a = int(input("input a side length of a cube: "))
+
+print(cube(a))
+"""
---
+++ cube_volume_test.py
@@ -0,0 +1,19 @@
+import unittest
+import cube_volume
+
+def test_cube_volume(self):
+    result = test_cube_volume.cube(1)
+    self.assertEqual(result, 2)
+
+class testCaseCube(unittest.TestCase):
+    def test_cube(self):
+        self.assertEqual(cube_volume.cube(3), 27)
+
+    def test_cube_02(self):
+        self.assertEqual(cube_volume.cube(1), 1)
+
+    def test_cube_03(self):
+        self.assertEqual(cube_volume.cube(-1), -1)
+
+if __name__ == '__main__':
+    unittest.main()
